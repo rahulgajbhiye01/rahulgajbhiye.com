59e0540bd65d25f13f78f8a20118cbc44115e6a2
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2024-01-09 21:52:28 +0530

    updates

--- src/constants/WorksData.js
+++ src/constants/WorksData.js
@@ -4,13 +4,16 @@ const WorksData = [
     img: new URL("../assets/linktree.png", import.meta.url).href,
     project_name: "Personalized Linktree",
     git_link: "https://github.com/rahulgajbhiye201/links.rahulgajbhiye",
+    project_link: "https://links.rahulgajbhiye.com/",
     details:
       "Personalize Linktree to represent who you are and share what you are passionate about.",
   },
   {
     scope: "Featured",
+    img: new URL("../assets/linktree.png", import.meta.url).href,
     project_name: "Next Project",
-    git_link: "https://github.com/rahulgajbhiye201",
+    git_link: "https://github.com/rahulgajbhiye201/links.rahulgajbhiye",
+    project_link: "https://links.rahulgajbhiye.com/",
     details: "",
   },
 ];
