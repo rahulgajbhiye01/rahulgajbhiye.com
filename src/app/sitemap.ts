c6a078615224a35b2cec8909d4518c9b02f913d9
Rahul Gajbhiye <rahulgajbhiye201@gmail.com>
2024-02-22 02:28:34 +0530

    sitemap update

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,6 +3,9 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+    },
+    {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/projects`,
     },
     {
